478e3ccc2fd10730221421078f8a580716ad06e3
vaarvik <johannvaarvik@hotmail.com>
2021-01-21 22:20:54 +0100

    Wordpress setup and plugin installation

---
+++ wp-content/plugins/gutenblocks/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'e08a449a18158123ca73913ba9c348c6');
---
+++ wp-content/themes/mytheme/gulp-config.js
@@ -0,0 +1,20 @@
+/**
+ *
+ *  ██████╗ ██╗   ██╗██╗     ██████╗      ██████╗ ██████╗ ███╗   ██╗███████╗██╗ ██████╗
+ * ██╔════╝ ██║   ██║██║     ██╔══██╗    ██╔════╝██╔═══██╗████╗  ██║██╔════╝██║██╔════╝
+ * ██║  ███╗██║   ██║██║     ██████╔╝    ██║     ██║   ██║██╔██╗ ██║█████╗  ██║██║  ███╗
+ * ██║   ██║██║   ██║██║     ██╔═══╝     ██║     ██║   ██║██║╚██╗██║██╔══╝  ██║██║   ██║
+ * ╚██████╔╝╚██████╔╝███████╗██║         ╚██████╗╚██████╔╝██║ ╚████║██║     ██║╚██████╔╝
+ *  ╚═════╝  ╚═════╝ ╚══════╝╚═╝          ╚═════╝ ╚═════╝ ╚═╝  ╚═══╝╚═╝     ╚═╝ ╚═════╝
+ *
+ * This file contains:
+ * * Settings for gulp.
+*/
+
+//add the proxy if you're using a server as for example WAMP
+module.exports.config = {
+	"proxy": 			"mytheme.test",
+	"port": 			8080,
+	"assetsUri": 		"assets", //path to the assets folder
+	"gutenBlocksName":	"gutenblocks" //the name of the Gutenberg blocks plugin you use
+}
