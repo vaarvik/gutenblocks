e45b05687c83e13f1213bd83a40f01c9b0a8fa70
vaarvik <johannvaarvik@hotmail.com>
2021-02-14 10:07:52 +0100

    Changed and imported fonts

--- wp-content/themes/mytheme/inc/enqueue-styles.php
+++ wp-content/themes/mytheme/inc/enqueue-styles.php
@@ -6,8 +6,10 @@
 
 function mytheme_register_styles() {
 
+	wp_enqueue_style( 'gf-open-sans', 'https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap', array(), THEME_VERSION );
+	wp_enqueue_style( 'gf-lato', 'https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap', array(), THEME_VERSION );
 	wp_enqueue_style( 'mytheme-info', get_stylesheet_uri(), array(), THEME_VERSION );
-	wp_enqueue_style( 'mytheme-style', mytheme_asset_url( "assets/styles/style.css" ), array(), THEME_VERSION );
+	wp_enqueue_style( 'mytheme-style', mytheme_asset_url( "assets/styles/style.css" ), array( 'gf-open-sans' ), THEME_VERSION );
 
 }
 add_action( 'wp_enqueue_scripts', 'mytheme_register_styles' );
@@ -27,7 +29,7 @@ add_action( 'admin_enqueue_scripts', 'mytheme_register_admin_styles' );
  */
 function mytheme_register_editor_styles() {
 
-	wp_enqueue_style( 'mytheme-editor-style', mytheme_asset_url( "assets/styles/editor.css" ), array(), THEME_VERSION );
+	wp_enqueue_style( 'mytheme-editor-style', mytheme_asset_url( "assets/styles/editor.css" ), array( 'google-fonts' ), THEME_VERSION );
 
 }
 add_action( 'enqueue_block_editor_assets', 'mytheme_register_editor_styles' );
