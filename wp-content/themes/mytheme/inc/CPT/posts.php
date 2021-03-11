c5ed734c237d56457b7d284d6988626b8589f0fc
vaarvik <johannvaarvik@hotmail.com>
2021-03-11 23:56:52 +0100

    Posts have props repeater

--- wp-content/themes/mytheme/inc/CPT/posts.php
+++ wp-content/themes/mytheme/inc/CPT/posts.php
@@ -18,3 +18,27 @@ new mt_meta_field( array(
 	'title'		=> __('Reference URL'),
 	'location'	=> array( 'post' ),
 ) );
+
+new mt_meta_field( array(
+	'type'		=> 'repeater',
+	'slug'		=> 'props',
+	'class'		=> 'field input',
+	'title'		=> __( 'Props' ),
+	'location'	=> array( 'post' ),
+	'fields'	=> array(
+		array(
+			'type'		=> 'input',
+			'slug'		=> 'text',
+			'class'		=> 'field input',
+			'title'		=> __( 'Text' ),
+			'location'	=> array( 'post' ),
+		),
+		array(
+			'type'		=> 'input',
+			'slug'		=> 'text',
+			'class'		=> 'field input',
+			'title'		=> __( 'Popover' ),
+			'location'	=> array( 'post' ),
+		),
+	),
+) );
