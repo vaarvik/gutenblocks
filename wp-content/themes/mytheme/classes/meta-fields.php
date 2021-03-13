d9b4cad316a2858c8b848be9ca1807b83c7cb1f0
vaarvik <johannvaarvik@hotmail.com>
2021-03-13 11:52:20 +0100

    Last commit was more a meta field group creation. Now meta-fields can be proper repeaters (still without JS). Also modified the group field type a bit.

--- wp-content/themes/mytheme/classes/meta-fields.php
+++ wp-content/themes/mytheme/classes/meta-fields.php
@@ -15,23 +15,45 @@ class mt_meta_field {
 	public function __construct( $args, $is_inner_field = false ) {
 		$this->args = $args;
 
-		if( !$is_inner_field ) {
-			add_action( 'add_meta_boxes', [ $this, 'create_field' ] );
-
-			add_action( 'save_post', function( $post_id ) {
-				$this->save_field( $post_id );
-			} );
-		}
-
 		//saving values for repeaters and groups
 		if( !empty( $this->args['fields'] ) ) {
-			foreach ( $this->args['fields'] as $key => $field ) {
-				$field['slug']	= $this->args['slug'] . "__" . $field['slug'];
-				$field 			= new mt_meta_field( $field, true );
-				$this->args['fields'][$key] = $field;
-				add_action( 'save_post', function( $post_id ) use ( $field ) {
-					$field->save_field( $post_id );
-				} );
+			if( $this->args['type'] === "repeater" ) {
+				add_action( 'save_post', [ $this, 'save_repeater_field' ] );
+			} elseif( $this->args['type'] === "group" ) {
+				add_action( 'save_post', [ $this, 'save_group_field' ] );
+			}
+		}
+
+		if( !$is_inner_field ) {
+			add_action( 'add_meta_boxes', [ $this, 'create_field' ] );
+			add_action( 'save_post', [ $this, 'save_field' ] );
+		}
+	}
+
+	public function save_group_field( $post_id, $args = null ) {
+		if( !$args ) $args = $this->args;
+		foreach ( $args['fields'] as $key => $field ) {
+			$field['slug'] = $args['slug'] . "__" . $field['slug'];
+			$args['fields'][$key]['slug'] = $field['slug'];
+			$this->save_field( $post_id, $field['slug'] );
+			if( !empty( $field['fields'] ) ) {
+				$this->save_group_field( $post_id, $field );
+			}
+		}
+	}
+
+	public function save_repeater_field( $post_id, $args = null ) {
+		if( !$args ) $args = $this->args;
+		foreach ( $args['fields'] as $key => $field ) {
+			$repeater_json 	= get_post_meta( $post_id, $args['slug'], true ) ? get_post_meta( get_the_ID(), $args['slug'], true ) : '[0]';
+			$repeater_ids	= json_decode( $repeater_json );
+			foreach ( $repeater_ids as $id ) {
+				$field['slug'] = $args['slug'] . "_" . $id . "__" . $field['slug'];
+				$args[$key]['slug'] = $field['slug'];
+				$this->save_field( $post_id, $field['slug'] );
+			}
+			if( !empty( $field['fields'] ) ) {
+				$this->save_repeater_field( $post_id, $field );
 			}
 		}
 	}
@@ -40,15 +62,16 @@ class mt_meta_field {
 		add_meta_box( $this->args['slug'], $this->args['title'], [ $this, 'create_field_html' ], $this->args['location'] );
 	}
 
-	public function save_field( $post_id ) {
-		if( isset( $_POST[$this->args['slug']] ) ) {
-			$meta_value = sanitize_text_field( $_POST[$this->args['slug']] );
+	public function save_field( $post_id, $slug = null ) {
+		if( !$slug ) $slug = $this->args['slug'];
+		if( isset( $_POST[$slug] ) ) {
+			$meta_value = sanitize_text_field( $_POST[$slug] );
 			if( $meta_value ) {
-				update_post_meta( $post_id, $this->args['slug'], $meta_value );
+				update_post_meta( $post_id, $slug, $meta_value );
 			}
 			else {
-				$old_meta_value = get_post_meta( get_the_ID(), $this->args['slug'], true );
-				delete_post_meta( $post_id, $this->args['slug'], $old_meta_value );
+				$old_meta_value = get_post_meta( get_the_ID(), $slug, true );
+				delete_post_meta( $post_id, $slug, $old_meta_value );
 			}
 		}
 	}
@@ -86,12 +109,33 @@ class mt_meta_field {
 					endforeach;
 				echo "</select>";
 				break;
-			case "repeater" :
-				echo '<div class="repeater ' . $class_name . '" name="' . $slug . '" id="' . $slug . '">';
+			case "group" :
+				echo '<div class="group ' . $class_name . '" name="' . $slug . '" id="' . $slug . '">';
 					foreach ($this->args['fields'] as $key => $field) {
-						$field->create_field_html();
+						$field_object = new mt_meta_field( $field, true );
+						$field_object->args['slug'] = $this->args['slug'] . "__" . $field['slug'];
+						$field_object->create_field_html();
 					}
 				echo "</div>";
+				break;
+			case "repeater" :
+				$repeater_json 	= get_post_meta( get_the_ID(), $slug, true ) ? get_post_meta( get_the_ID(), $slug, true ) : '[0]';
+				$repeater_ids	= json_decode( $repeater_json ); //array( 0, 1, 3 );
+					echo '<div class="repeater ' . $class_name . '" id="' . $slug . '">';
+
+						foreach ( $repeater_ids as $key => $id ) {
+							echo '<div class="repeater__item" id="' . $slug . "_" . $id . '">';
+								foreach ( $this->args['fields'] as $key => $field ) {
+									$field_object = new mt_meta_field( $field, true );
+									$field_object->args['slug'] = $this->args['slug'] . "_" . $id . "__" . $field['slug'];
+									$field_object->create_field_html();
+								}
+							echo "</div>";
+						}
+
+						echo '<input type="hidden" name="' . $slug . '" id="' . $slug . '" value="' . $repeater_json . '"/>';
+						echo '<button class="repeater__btn btn">Add</button>';
+					echo "</div>";
 				break;
 		}
 	}
